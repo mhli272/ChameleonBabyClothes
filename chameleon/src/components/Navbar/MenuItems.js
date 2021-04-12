15520b9ac1ac127c36904992faea49b09a68949c
Matthew Li <mhli@usc.edu>
2021-04-11 18:56:02 -0700

    basic navbar

---
+++ chameleon/src/components/Navbar/MenuItems.js
@@ -0,0 +1,37 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Shop",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "HIW",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Community",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Search",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Cart",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "Profile",
+        url: "#",
+        cName: "nav-links"
+    },
+]

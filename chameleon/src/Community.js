1d9b39b19be2376142c164c368f5f6e9b58a323b
Matthew Li <mhli@usc.edu>
2021-04-28 17:55:35 -0700

    finished community page

---
+++ chameleon/src/Community.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from "react-router-dom";
+import './Community.css';
+import { Redirect } from "react-router-dom";
+
+import community1 from './images/community_post_1.png';
+import community2 from './images/community_post_2.png';
+import community3 from './images/community_post_3.png';
+import community4 from './images/community_post_4.png';
+
+import Footer from './components/Footer/Footer.js';
+import Navbar from './components/Navbar/Navbar.js';
+
+export class Community extends React.Component {
+
+    render() {
+        return (
+            <fragment>
+                {/*header*/}
+                <div>
+                    <Navbar ></Navbar>
+                </div>
+
+                <div class="marginFix"> </div>
+
+                <div class="title">Community</div>
+                <div class="subtitle">We want Chameleon to be a place for parents to share their clothing and tips and tricks for taking care of their newborns</div>
+                <div class="community-img"><img src={community1}></img></div>
+                <div class="community-img"><img src={community2}></img></div>
+                <div class="community-img"><img src={community3}></img></div>
+                <div class="community-img"><img src={community4}></img></div>
+                <div class="see-more-text">See more posts</div>
+                {/*footer*/}
+                <div>
+                    <Footer></Footer>
+                </div>
+            </fragment>
+
+
+        );
+    }
+}
--- chameleon/src/index.js
+++ chameleon/src/index.js
@@ -8,6 +8,7 @@ import {Shop} from './Shop.js'
 import {HowItWorks} from './HowItWorks.js'
 import {Favorites} from './Favorites.js'
 import {Product} from './Product.js'
+import {Community} from './Community.js'
 
 export default function App() {
 
@@ -19,6 +20,7 @@ export default function App() {
   <Route path="/" exact component={Main} />
   <Route path="/shop" component={Shop} />
   <Route path="/how-it-works" component={HowItWorks} />
+  <Route path="/community" component={Community} />
   <Route path="/favorites" component={Favorites} />
   <Route path="/product" component={Product} />
     </main>
